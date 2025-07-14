be2f013ef0614831e29e5a82328544f518245e0a
Tayssa Ramos <tayssaramos4@gmail.com>
2025-07-13 23:13:32 -0300

    Delete src/schemas/usuario.schema.js

--- src/schemas/usuario.schema.js
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const criarUsuarioSchema = z.object({
-  nome: z.string().min(1, "O nome é obrigatório"),
-  email: z.string().email("E-mail inválido"),
-  senha: z.string().min(6, "A senha deve ter no mínimo 6 caracteres")
-});
